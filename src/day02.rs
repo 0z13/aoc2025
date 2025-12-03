5818c6a22c87e2fa4febe0d9f0dee7e95020b890
Jonathan Zielinski <jozi@dbc.dk>
2025-12-03 20:49:37 +0100

    part 2 day 2
    yes generate all ids
    
    100k invalid_ids from 1-9 billion
    
    very annoying problem, can you do this without?
    without string generation
    
    TODO: fix part 1 bruteforce

--- src/day02.rs
+++ src/day02.rs
@@ -1,4 +1,5 @@
 use std::{ops::RangeInclusive};
+use std::collections::HashSet;
 // I'll do it in the dumbest possible way first,
 // and revisit later. No good ideas right now.
 pub fn part_1(ranges: &Vec<RangeInclusive<usize>>) -> usize {
@@ -16,9 +17,59 @@ pub fn part_1(ranges: &Vec<RangeInclusive<usize>>) -> usize {
     counter
 }
 
-pub fn part_2(ranges: &Vec<RangeInclusive<usize>>) -> usize {
-    1337
+fn invalid_ids(seed_limit: usize, max: usize) -> HashSet<usize> {
+    let mut all_invalid_ids = HashSet::new();
+
+    for n in 1..seed_limit {
+        let id = n.to_string();
+
+        let mut current = id.clone();
+
+        loop {
+            current.push_str(&id);
+
+            if current.len() > max.to_string().len() {
+                break;
+            }
+
+            let id_val = current.parse::<usize>().expect("heyho");
+
+            if id_val > max {
+                break;
+            }
+
+            all_invalid_ids.insert(id_val);
+        }
+    }
+
+    all_invalid_ids
 }
+
+pub fn part_2(ranges: &Vec<RangeInclusive<usize>>) -> usize {
+    let max = ranges.iter()
+        .map(|range| range.end())
+        .max()
+        .unwrap();
+
+    let strlen = max.to_string().len();
+
+    let seed_limit = 10_usize.pow((strlen as u32 / 2) + 2);
+
+    let h = invalid_ids(seed_limit, *max);
+
+    let mut result = 0;
+    for invalid_id in h {
+        for range in ranges {
+            if range.contains(&invalid_id) {
+                result += invalid_id;
+                break;
+            }
+        }
+    }
+
+    result
+}
+
 
 pub fn parse(input: &'static str) -> Vec<RangeInclusive<usize>> {
     let mut ranges = vec![];
