5f164d1f862b91297af0fba077bef46db0820044
Jonathan Zielinski <jozi@dbc.dk>
2025-12-05 00:16:59 +0100

    day 3 pt 2

--- src/day03.rs
+++ src/day03.rs
@@ -18,10 +18,33 @@ pub fn part_1(data: &Vec<Vec<u32>>)  -> usize {
     }
     result
 }
+pub fn part_2(data: &Vec<Vec<u32>>) -> u64 {
+    let mut v = vec![];
 
-pub fn part_2(data: &Vec<Vec<u32>>) -> usize{
+    for bat in data {
+        let mut right_start = 0;
+        let mut search = 12;
+        let mut result: u64 = 0;
 
-    1337
+        while search > 0 {
+            let end_bound = bat.len() - search + 1;
+            let slice = &bat[right_start..end_bound];
+
+            let (new_index, value) = slice.iter()
+                .enumerate()
+                .rev() // max_by_key returns the _last_ maximum number on ties,
+                       // we want the first to increase window
+                .max_by_key(|(_idx, val)| *val)
+                .unwrap();
+
+            result = result * 10 + (*value as u64);
+
+            right_start += new_index + 1;
+            search -= 1;
+        }
+        v.push(result);
+    }
+    v.iter().sum()
 }
 
 pub fn parse(input: &str) -> Vec<Vec<u32>> {
